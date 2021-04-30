e5b32c104c845135417af6d3caa5bbd1cbef9579
Osvaldo Martini <OsvaldoMartini@users.noreply.github.com>
2021-04-30 15:45:17 +0200

    Rest Assured

--- src/test/java/com/training/restassured/Rest_Api_Tests.java
+++ src/test/java/com/training/restassured/Rest_Api_Tests.java
@@ -12,7 +12,7 @@ public class Rest_Api_Tests {
 	private static final String USER_ID = "9b5f49ab-eea9-45f4-9d66-bcf56a531b85";
 	 private static final String USERNAME = "osvaldo.martini@gmail.com";
 	 private static final String PASSWORD = "martini";
-	 private static final String BASE_URL = "https://bookstore.toolsqa.com";
+	 private static final String BASE_URL = "http://127.0.0.1:9095";
 	 
 	 private static String token;
 	 private static Response response;
@@ -21,6 +21,19 @@ public class Rest_Api_Tests {
 	
   @Test
   public static void getResponseBody(){
+	  
+	  RestAssured.baseURI = BASE_URL;
+	  RequestSpecification request = RestAssured.given();
+	  
+	  request.header("Content-Type", "application/json");
+	  response = request.body("{ \"username\":\"" + USERNAME + "\", \"password\":\"" + PASSWORD + "\"}")
+	  .post("/api/users/login/Project-Template");
+	  
+	  String jsonString = response.asString();
+	  token = JsonPath.from(jsonString).get("token");
+	  
+	  System.out.println(token);	
+	  
 	  
 //	  given().queryParam("CUSTOMER_ID","68195")
 //	           .queryParam("PASSWORD","1234!")
