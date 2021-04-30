94694f60a32b957b0ad50409ad8ac47fb34c7fc0
Osvaldo Martini <OsvaldoMartini@users.noreply.github.com>
2021-04-30 16:20:10 +0200

    Cucumber Added

---
+++ src/test/java/com/training/cucumber/LoginTestRunner.java
@@ -0,0 +1,14 @@
+package com.training.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features", glue="stepImplementations")
+public class LoginTestRunner {
+	
+
+}
